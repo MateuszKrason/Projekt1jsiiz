0af67bfd894cd4b3d774e5208804172df755f95b
Mateusz <s180450@student.edu.pl>
2023-03-12 17:38:30 +0100

    newer version

--- cpp_implementation.cpp
+++ cpp_implementation.cpp
@@ -2,12 +2,11 @@
 #include <numeric>
 
 int funct(int a, int b) {
+    // int a = 462;
+    // int b = 1071;
     int result = std::gcd(a, b);
 
     std::cout << result << std::endl;
 
-    return 0;
-}
-int main(){
-    
+    return result;
 }
--- test_poprawnosci.py
+++ test_poprawnosci.py
@@ -11,16 +11,16 @@ for i in range(num_tests):
     test_data.append((a, b))
 
 for i, data in enumerate(test_data):
-    with open(f"test_{i+1}.txt", "w") as f:
-        f.write(f"{data[0]} {data[1]}")
+    with open("test_" + str(i+1) +".txt", "w") as f:
+        f.write("{data[0]} {data[1]}")
 
-    with open(f"python_{i+1}.txt", "w") as f:
+    with open("python_"+ str(i+1) + ".txt", "w") as f:
         result = math.gcd(data[0], data[1])
         f.write(str(result))
 
-    system("g++ -o cpp gcd.cpp")
-    with open(f"cpp_{i+1}.txt", "w") as f:
-        system(f"./cpp {data[0]} {data[1]} > output.txt")
+    system("g++ -o cpp cpp_implementation.cpp")
+    with open("cpp_" +str(i+1) + ".txt", "w") as f:
+        system("cpp {data[0]} {data[1]} > output.txt")
         with open("output.txt", "r") as f2:
             result = f2.read().strip()
             f.write(result)
